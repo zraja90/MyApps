74189d63872b341db0a2ba094bd8002825786c13
zraja90 <zeeshan@unigo.com>
2012-11-26 14:34:13 -0500

    updates

---
+++ ToolDepot/Services/UnderConstructionService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ToolDepot.Data;
+using ToolDepot.Models;
+
+namespace ToolDepot.Services
+{
+    public class UnderConstructionService : CrudService<UnderConstructionModel>, IUnderConstructionService
+    {
+        public UnderConstructionService(IRepository<UnderConstructionModel> repo)
+            : base(repo)
+        {
+            
+        }
+    }
+}
