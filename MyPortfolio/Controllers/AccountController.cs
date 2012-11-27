625782f35c52a81732e93a49cda203f6acb9cb34
zraja90 <zeeshan@unigo.com>
2012-11-27 13:43:48 -0500

    updated autofac for all projects

--- MyPortfolio/Controllers/AccountController.cs
+++ MyPortfolio/Controllers/AccountController.cs
@@ -264,23 +264,23 @@ namespace MyPortfolio.Controllers
                 // Insert a new user into the database
                 using (UsersContext db = new UsersContext())
                 {
-                    UserProfile user = db.UserProfiles.FirstOrDefault(u => u.UserName.ToLower() == model.UserName.ToLower());
-                    // Check if user already exists
-                    if (user == null)
-                    {
-                        // Insert name into the profile table
-                        db.UserProfiles.Add(new UserProfile { UserName = model.UserName });
-                        db.SaveChanges();
+                    //UserProfile user = db.UserProfiles.FirstOrDefault(u => u.UserName.ToLower() == model.UserName.ToLower());
+                    //// Check if user already exists
+                    //if (user == null)
+                    //{
+                    //    // Insert name into the profile table
+                    //    db.UserProfiles.Add(new UserProfile { UserName = model.UserName });
+                    //    db.SaveChanges();
 
-                        OAuthWebSecurity.CreateOrUpdateAccount(provider, providerUserId, model.UserName);
-                        OAuthWebSecurity.Login(provider, providerUserId, createPersistentCookie: false);
+                    //    OAuthWebSecurity.CreateOrUpdateAccount(provider, providerUserId, model.UserName);
+                    //    OAuthWebSecurity.Login(provider, providerUserId, createPersistentCookie: false);
 
-                        return RedirectToLocal(returnUrl);
-                    }
-                    else
-                    {
-                        ModelState.AddModelError("UserName", "User name already exists. Please enter a different user name.");
-                    }
+                    //    return RedirectToLocal(returnUrl);
+                    //}
+                    //else
+                    //{
+                    //    ModelState.AddModelError("UserName", "User name already exists. Please enter a different user name.");
+                    //}
                 }
             }
 
