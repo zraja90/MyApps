80453b8dec26671848afdf276bee12e2521ea83c
Zeeshan Raja <zeeshan@unigo.com>
2012-12-25 16:05:11 -0500

    updates

--- ToolDepot/App_Start/BundleConfig.cs
+++ ToolDepot/App_Start/BundleConfig.cs
@@ -29,7 +29,8 @@ namespace ToolDepot
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
             //bundles.Add(new StyleBundle("~/Content/assets/bootstrap").Include("~/assets/css/bootstrap.min.css"));
 
-            var less = new StyleBundle("~/Content/less").Include("~/Content/css/site.less","~/Content/bootstrap/bootstrap.less");
+            var less = new StyleBundle("~/Content/less").Include("~/Content/css/site.less","~/Content/bootstrap/bootstrap.less",
+                "~/Content/css/alignment.less");
             
             less.Transforms.Add(new LessMinify());
 
